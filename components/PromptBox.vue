2e2c7e803da1b67c7fcbfdc281c23857e6a401b1
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-05-16 14:57:05 +0800

    feat: skeleton of PromptBox

---
+++ components/PromptBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="space-y-2">
+    <!--bar-->
+    <div class="flex flex-row justify-between">
+      <div class="flex flex-row gap-2">
+        <UButton icon="i-mingcute-brain-line" color="gray" />
+        <UButton icon="i-mingcute-thermometer-line" color="gray" />
+        <UButton icon="i-mingcute-pic-line" color="gray" />
+        <UButton icon="i-mingcute-mic-line" color="gray" />
+      </div>
+      <div class="flex flex-row gap-2">
+        <UButton icon="i-mingcute-eraser-line" color="gray" />
+      </div>
+    </div>
+    <!--text area-->
+    <div>
+      <UTextarea
+        placeholder="Type your message here ..."
+        variant="none"
+        :padded="false"
+      />
+    </div>
+    <!--send button-->
+    <div class="flex flex-row justify-end">
+      <UButtonGroup orientation="horizontal">
+        <UButton label="Send" />
+        <UButton icon="i-heroicons-chevron-down-20-solid" />
+      </UButtonGroup>
+    </div>
+  </div>
+</template>
