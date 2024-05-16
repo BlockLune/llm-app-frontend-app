43158baec84fefa750e3f52de1684546d85f9e04
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-05-16 15:54:29 +0800

    feat: skeleton of chat page

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-10">
+    <div class="flex gap-4">
+      <div class="w-1/4">
+        <SideBar />
+      </div>
+      <div class="w-3/4 flex flex-col justify-between">
+        <div class="h-2/3"></div>
+        <PromptBox class="h-1/3" />
+      </div>
+    </div>
+  </div>
+</template>
